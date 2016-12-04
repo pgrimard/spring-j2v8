09b5d921e803b3d174a5b4d3a4d71ba14a7693ec
Patrick Grimard <pgrimard@gmail.com>
2016-12-04 15:45:45 -0500

    V8ScriptTemplateView renderMergedOutputModel will now use some helper functions to convert a Java Map<String, Object> to a V8Object with deep property conversion.

--- src/main/java/com/patrickgrimard/examples/V8ScriptTemplateView.java
+++ src/main/java/com/patrickgrimard/examples/V8ScriptTemplateView.java
@@ -1,6 +1,7 @@
 package com.patrickgrimard.examples;
 
 import com.eclipsesource.v8.V8;
+import com.eclipsesource.v8.V8Array;
 import com.eclipsesource.v8.V8Object;
 import com.eclipsesource.v8.V8Value;
 import org.springframework.beans.BeansException;
@@ -139,8 +140,7 @@ public class V8ScriptTemplateView extends AbstractUrlBasedView {
                 .collect(toList());
 
         String template = getResourceAsString(getUrl());
-        V8Object modelAttributes = new V8Object(v8);
-        model.forEach((k, v) -> modelAttributes.add(k, (String) v)); //TODO Values may not always be String values
+        V8Object modelAttributes = modelToV8Object(v8, model);
 
         Object html = v8.executeJSFunction("render", template, modelAttributes);
         response.getWriter().write(String.valueOf(html));
@@ -148,6 +148,70 @@ public class V8ScriptTemplateView extends AbstractUrlBasedView {
         modelAttributes.release();
         scriptResults.forEach(V8Value::release);
         v8.release();
+    }
+
+    private V8Object modelToV8Object(V8 v8, Map<String, Object> model) {
+        V8Object result = new V8Object(v8);
+
+        model.forEach((key, object) -> {
+            if (object == null) {
+                result.addNull(key);
+            } else if (object instanceof V8Value) {
+                result.add(key, (V8Value) object);
+            } else if (object instanceof Integer) {
+                result.add(key, (Integer) object);
+            } else if (object instanceof Double) {
+                result.add(key, (Double) object);
+            } else if (object instanceof Long) {
+                result.add(key, ((Long) object).doubleValue());
+            } else if (object instanceof Float) {
+                result.add(key, (Float) object);
+            } else if (object instanceof Boolean) {
+                result.add(key, (Boolean) object);
+            } else if (object instanceof String) {
+                result.add(key, (String) object);
+            } else if (object instanceof Map) {
+                result.add(key, modelToV8Object(v8, (Map<String, Object>) object));
+            } else if (object instanceof Iterable) {
+                result.add(key, modelToV8Array(v8, (Iterable<?>) object));
+            } else {
+                throw new IllegalArgumentException("Unsupported Object of type: " + object.getClass());
+            }
+        });
+
+        return result;
+    }
+
+    private V8Array modelToV8Array(V8 v8, Iterable<?> iterable) {
+        V8Array result = new V8Array(v8);
+
+        iterable.forEach(object -> {
+            if (object == null) {
+                result.pushNull();
+            } else if (object instanceof V8Value) {
+                result.push((V8Value) object);
+            } else if (object instanceof Integer) {
+                result.push((Integer) object);
+            } else if (object instanceof Double) {
+                result.push((Double) object);
+            } else if (object instanceof Long) {
+                result.push(((Long) object).doubleValue());
+            } else if (object instanceof Float) {
+                result.push((Float) object);
+            } else if (object instanceof Boolean) {
+                result.push((Boolean) object);
+            } else if (object instanceof String) {
+                result.push((String) object);
+            } else if (object instanceof Map) {
+                result.push(modelToV8Object(v8, (Map<String, Object>) object));
+            } else if (object instanceof Iterable) {
+                result.push(modelToV8Array(v8, (Iterable<?>) object));
+            } else {
+                throw new IllegalArgumentException("Unsupported Object of type: " + object.getClass());
+            }
+        });
+
+        return result;
     }
 
     protected Resource getResource(String location) {
