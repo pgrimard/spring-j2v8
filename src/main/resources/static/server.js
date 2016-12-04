4a6b12f6d32f01241a62d2062b2502b13684f9cc
Patrick Grimard <pgrimard@gmail.com>
2016-12-04 15:44:25 -0500

    server.js will now replace SERVER_RENDERED_STATE with model.initialState

--- src/main/resources/static/server.js
+++ src/main/resources/static/server.js
@@ -1,3 +1,14 @@
+/**
+ * Server side render function that merges a template with a model.  Caution should be taken when replacing template
+ * variables with model data to avoid script injection attacks.  Consider using serialize-javascript to avoid these
+ * attacks {@link https://www.npmjs.com/package/serialize-javascript}.
+ *
+ * @param template The template that will be merged.
+ * @param model The model data to merge with the template.
+ * @returns {string} The merged template.
+ */
 window.render = function render(template, model) {
-  return template.replace('SERVER_RENDERED_HTML', JSON.stringify(model));
+  return template
+    .replace('SERVER_RENDERED_HTML', model.req)
+    .replace('SERVER_RENDERED_STATE', model.initialState);
 };
