f0ee2337fd19f7ff6ae16a1484bce1c0436eb46b
Patrick Grimard <pgrimard@gmail.com>
2016-12-04 12:57:08 -0500

    Cleanup

--- src/main/java/com/patrickgrimard/examples/SpringJ2V8Application.java
+++ src/main/java/com/patrickgrimard/examples/SpringJ2V8Application.java
@@ -7,17 +7,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringJ2V8Application {
 
-	@Bean
-	public V8ScriptTemplateViewResolver viewResolver() {
-		return new V8ScriptTemplateViewResolver("/static/", ".html");
-	}
-
-	@Bean
-    public V8ScriptTemplateConfigurer v8ScriptTemplateConfigurer() {
-	    return new V8ScriptTemplateConfigurer("static/server.js");
+    @Bean
+    public V8ScriptTemplateViewResolver viewResolver() {
+        return new V8ScriptTemplateViewResolver("/static/", ".html");
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJ2V8Application.class, args);
-	}
+    @Bean
+    public V8ScriptTemplateConfigurer v8ScriptTemplateConfigurer() {
+        return new V8ScriptTemplateConfigurer("static/server.js");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJ2V8Application.class, args);
+    }
 }
