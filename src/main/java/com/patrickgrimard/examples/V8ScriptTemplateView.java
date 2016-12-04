d77bddee6b6e89e13fe8a71ce506aac67ff23209
Patrick Grimard <pgrimard@gmail.com>
2016-12-04 17:02:33 -0500

    V8ScriptTemplateView now keeps track of any newly created V8Value instances such as V8Object, V8Array, etc so that they can all be explicitly released prior to releaseing the V8 runtime itself.
    Added support for arrays as model attributes which will be converted to V8Array instances.

--- src/main/java/com/patrickgrimard/examples/V8ScriptTemplateView.java
+++ src/main/java/com/patrickgrimard/examples/V8ScriptTemplateView.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -129,6 +130,8 @@ public class V8ScriptTemplateView extends AbstractUrlBasedView {
     protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         V8 v8 = V8.createV8Runtime("window");
 
+        List<V8Value> runtimeObjects = new ArrayList<>();
+
         List<V8Value> scriptResults = Arrays.stream(this.scripts)
                 .map(script -> {
                     try {
@@ -138,19 +141,20 @@ public class V8ScriptTemplateView extends AbstractUrlBasedView {
                     }
                 })
                 .collect(toList());
+        runtimeObjects.addAll(scriptResults);
 
         String template = getResourceAsString(getUrl());
-        V8Object modelAttributes = mapToV8Object(v8, model);
+        V8Object modelAttributes = mapToV8Object(v8, runtimeObjects, model);
+        runtimeObjects.add(modelAttributes);
 
         Object html = v8.executeJSFunction("render", template, modelAttributes);
         response.getWriter().write(String.valueOf(html));
 
-        modelAttributes.release();
-        scriptResults.forEach(V8Value::release);
+        runtimeObjects.forEach(V8Value::release);
         v8.release();
     }
 
-    private V8Object mapToV8Object(V8 v8, Map<String, Object> model) {
+    private V8Object mapToV8Object(V8 v8, List<V8Value> runtimeObjects, Map<String, Object> model) {
         V8Object result = new V8Object(v8);
 
         model.forEach((key, object) -> {
@@ -171,18 +175,21 @@ public class V8ScriptTemplateView extends AbstractUrlBasedView {
             } else if (object instanceof String) {
                 result.add(key, (String) object);
             } else if (object instanceof Map) {
-                result.add(key, mapToV8Object(v8, (Map<String, Object>) object));
+                result.add(key, mapToV8Object(v8, runtimeObjects, (Map<String, Object>) object));
             } else if (object instanceof Iterable) {
-                result.add(key, modelToV8Array(v8, (Iterable<Object>) object));
+                result.add(key, modelToV8Array(v8, runtimeObjects, (Iterable<Object>) object));
+            } else if (object.getClass().isArray()) {
+                result.add(key, modelToV8Array(v8, runtimeObjects, Arrays.asList((Object[]) object)));
             } else {
                 throw new IllegalArgumentException("Unsupported Object of type: " + object.getClass());
             }
         });
 
+        runtimeObjects.add(result);
         return result;
     }
 
-    private V8Array modelToV8Array(V8 v8, Iterable<Object> iterable) {
+    private V8Array modelToV8Array(V8 v8, List<V8Value> runtimeObjects, Iterable<Object> iterable) {
         V8Array result = new V8Array(v8);
 
         iterable.forEach(object -> {
@@ -203,14 +210,17 @@ public class V8ScriptTemplateView extends AbstractUrlBasedView {
             } else if (object instanceof String) {
                 result.push((String) object);
             } else if (object instanceof Map) {
-                result.push(mapToV8Object(v8, (Map<String, Object>) object));
+                result.push(mapToV8Object(v8, runtimeObjects, (Map<String, Object>) object));
             } else if (object instanceof Iterable) {
-                result.push(modelToV8Array(v8, (Iterable<Object>) object));
+                result.push(modelToV8Array(v8, runtimeObjects, (Iterable<Object>) object));
+            } else if (object.getClass().isArray()) {
+                result.push(modelToV8Array(v8, runtimeObjects, Arrays.asList((Object[]) object)));
             } else {
                 throw new IllegalArgumentException("Unsupported Object of type: " + object.getClass());
             }
         });
 
+        runtimeObjects.add(result);
         return result;
     }
 
