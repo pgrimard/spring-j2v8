136684a1fbd85ce9eb742709ea011dda0ae124f6
Patrick Grimard <pgrimard@gmail.com>
2016-12-04 17:04:13 -0500

    Index controller no longer uses ObjectMapper to convert objects to Strings, instead it now demonstrates using native Java Map, Iterable, String or arrays.  Using ObjectMapper still works though since a result can simply be converted to a String before it's added to a model and then parsed later.

--- src/main/java/com/patrickgrimard/examples/IndexController.java
+++ src/main/java/com/patrickgrimard/examples/IndexController.java
@@ -1,7 +1,6 @@
 package com.patrickgrimard.examples;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,28 +20,21 @@ public class IndexController {
 
     @GetMapping
     public String index(Model model, HttpServletRequest request) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-
         Map<String, Object> req = new HashMap<>();
         String root = request.getServletPath().equals("/index.html") ? "/" : request.getServletPath();
         if(request.getQueryString() != null)
             req.put("location", String.format("%s?%s", root, request.getQueryString()));
         else
             req.put("location", root);
-        model.addAttribute("req", mapper.writeValueAsString(req));
+        model.addAttribute("req", req);
 
         Map<String, Object> initialState = new HashMap<>();
-        initialState.put("hello", "world");
-
         Map<String, Object> deep = new HashMap<>();
-        deep.put("deepProp", "deepValue");
-        deep.put("anotherProp", "anotherValue");
-        deep.put("anArray", new String[]{"hello", "world", "again"});
-        deep.put("anIterable", Arrays.asList("hello", "world", "yet again"));
+        deep.put("stringVal", "I am a String");
+        deep.put("arrayVal", new String[]{"I", "am", "an", "array"});
+        deep.put("iterableVal", Arrays.asList("I", "am", "an", "iterable"));
         initialState.put("deep", deep);
-
-        model.addAttribute("initialState", mapper.writeValueAsString(initialState));
-
+        model.addAttribute("initialState", initialState);
 
         return "index";
     }
